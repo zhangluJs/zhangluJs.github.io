b6bdfb05817dfee1901477d42f354f14c3fe56a5
zhangluJs <897268181@qq.com>
2018-12-25 11:31:00 +0800

    code review

--- src/page/header/index.js
+++ src/page/header/index.js
@@ -1,7 +1,11 @@
+/**
+ * @file 头部
+ */
+
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import { ColorPicker } from 'element-react';
-import { connect } from 'react-redux';
+import {ColorPicker} from 'element-react';
+import {connect} from 'react-redux';
 import './index.scss';
 
 class Header extends React.Component {
@@ -11,46 +15,48 @@ class Header extends React.Component {
     }
 
     // dispatch action 去改变颜色
-    handleSwitchColor (color) {
+    handleSwitchColor(color) {
         if (this.props.onSwitchColor) {
-            this.props.onSwitchColor(color)
+            this.props.onSwitchColor(color);
         }
     }
 
-    render () {
+    render() {
         const color2 = '#409eff';
         return (
-            <header className="header" style={{ background : this.props.themeColor }}>
-                <div className="container">
-                    <img className="logo" src="../../../static/favicon.png"/>
-                    <ul className="nav-list">
-                        <li className="list-item"><NavLink to="/content" activeClassName="active-item">内容</NavLink></li>
-                        <li className="list-item"><NavLink to="/about" activeClassName="active-item">关于我</NavLink></li>
-                        <li className="list-item">
+            <header className='header' style={{
+                background: this.props.themeColor
+            }}>
+                <div className='container'>
+                    <img className='logo' src='../../../static/favicon.png'/>
+                    <ul className='nav-list'>
+                        <li className='list-item'><NavLink to='/content' activeClassName='active-item'>内容</NavLink></li>
+                        <li className='list-item'><NavLink to='/about' activeClassName='active-item'>关于我</NavLink></li>
+                        <li className='list-item'>
                             <ColorPicker
                                 value={color2}
                                 onChange={this.handleSwitchColor.bind(color2)}>
                             </ColorPicker>
                         </li>
                     </ul>
-                </div> 
+                </div>
             </header>
-        )
+            );
     }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
     return {
         themeColor: state.themeColor
-    }
-}
-const mapDispatchToProps = (dispatch) => {
+    };
+};
+const mapDispatchToProps = dispatch => {
     return {
-        onSwitchColor: (color) => {
-            dispatch({ type: 'CHANGE_COLOR', themeColor: color })
+        onSwitchColor: color => {
+            dispatch({type: 'CHANGE_COLOR', themeColor: color});
         }
-    }
-}
+    };
+};
 
 Header = connect(mapStateToProps, mapDispatchToProps)(Header);
 
