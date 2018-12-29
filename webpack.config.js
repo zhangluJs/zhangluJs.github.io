d6ffc4f41fe399cc35ec3da76fc416a68fa07cbf
zhangluJs <897268181@qq.com>
2018-12-29 10:21:18 +0800

    code review

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,36 @@
+/**
+ * @file webpack 配置文件
+ * @author zhanglu
+ * @data 2018
+ */
+
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+
+    // 指定模式
     mode: 'development',
+
+    // 项目入口
     entry: path.join(__dirname, 'src/main.js'),
+
+    // 项目build后的文件地址
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'js/main.js'
     },
-    devtool:"source-map",
+
+    // 开发时助于debugger
+    devtool: 'source-map',
+
+    // webpack内置的node服务
     devServer: {
-        contentBase: "./",
+        contentBase: './',
         inline: true,
         port: 8080,
         compress: false,
@@ -35,9 +51,9 @@ module.exports = {
                 test: /\.css$/,
                 loader: 'style-loader!css-loader'
             },
-            {   
+            {
                 test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
-                loader: "file-loader"
+                loader: 'file-loader'
             },
             {
                 test: /\.scss$/,
@@ -47,25 +63,35 @@ module.exports = {
                     {
                         loader: 'css-loader'
                     },
-                    'sass-loader',
-                ],
+                    'sass-loader'
+                ]
             }
         ]
     },
     plugins: [
+
+        // 生成html文件
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'index.html',
             inject: 'body'
         }),
+
+        // 提取css
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
             filename: 'css/[name].[hash].css',
-            chunkFilename: 'css/[id].[hash].css',
+            chunkFilename: 'css/[id].[hash].css'
         }),
+
+        // webpack热更新，不需要手动刷新页面
         new webpack.HotModuleReplacementPlugin(),
+
+        // 清除dist文件
         new CleanWebpackPlugin(['dist']),
+
+        // 复制静态文件到build后的文件夹
         new CopyWebpackPlugin([
             {
                 from: path.join(__dirname, '/src/static'),
@@ -73,4 +99,4 @@ module.exports = {
             }
         ])
     ]
-}
+};
