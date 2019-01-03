e8130af2f8257a9df206bae7f471dbb59b5a8c97
zhangluJs <897268181@qq.com>
2019-01-03 11:02:47 +0800

    code review

--- src/page/app/index.js
+++ src/page/app/index.js
@@ -1,29 +1,37 @@
+/**
+ * @file 页面主题
+ */
+
 import React from 'react';
-import {HashRouter, Route, NavLink, Switch, Redirect} from 'react-router-dom';
+import {
+    HashRouter,
+    Route,
+    NavLink,
+    Switch,
+    Redirect
+} from 'react-router-dom';
 import Header from '../header/index.js';
 import Content from '../content/index.js';
 import About from '../about/index.js';
+
 import './index.scss';
 
-
-class App extends React.Component {
+export default class App extends React.Component {
     render() {
         return (
             <HashRouter>
-                <div className="app">
+                <div className='app'>
                     <Header />
-                    <main className="main">
+                    <main className='main'>
                         <Switch>
                             {/* <Route exact path="/" component={Content}></Route> */}
-                            <Route path="/content" component={Content}></Route>
-                            <Route path="/about" component={About}></Route>
+                            <Route path='/content' component={Content}></Route>
+                            <Route path='/about' component={About}></Route>
                             <Redirect from='/' to='/content/one'></Redirect>
                         </Switch>
                     </main>
                 </div>
             </HashRouter>
-        )
+        );
     }
 }
-
-export default App;
