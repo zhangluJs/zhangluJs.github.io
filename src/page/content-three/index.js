c1fa1efb50df29d188276e005d7d07c72f23d951
zhangluJs <897268181@qq.com>
2019-01-09 10:38:06 +0800

    code review

--- src/page/content-three/index.js
+++ src/page/content-three/index.js
@@ -1,19 +1,24 @@
+/**
+ * @file page-three
+ */
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
 class ContentThree extends React.Component {
-    render () {
+    render() {
         return (
-            <div style={{color: this.props.themeColor}}>第三年的跌宕起伏，幡然醒悟。目前在好好学习！</div>
-        )
+            <div style={{
+                color: this.props.themeColor
+            }}>第三年的跌宕起伏，幡然醒悟。目前在好好学习！</div>
+        );
     }
 }
 
-const mapStateToprops = (state) => {
+const mapStateToprops = state => {
     return {
         themeColor: state.themeColor
-    }
-}
+    };
+};
 
 ContentThree = connect(mapStateToprops)(ContentThree);
 
