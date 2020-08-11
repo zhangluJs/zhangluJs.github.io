3c2263767e034b12e3a6aa0d54307ae8897bf26d
zhangluJs <897268181@qq.com>
2020-08-11 11:55:14 +0800

    hang

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,11 +3,25 @@
  */
 
 import React from 'react';
+import './index.scss';
 
 export default class About extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
-            <div>这里是一个About的组件</div>
+            <div className="container about-wrap">
+                <img className="hang-photo" src="./static/img/IMG_0453.JPG"/>
+                <div className="mp3-icon">
+                    <audio controls="controls" autoPlay="autoplay">
+                        <source src="http://music.163.com/song/media/outer/url?id=1373002687.mp3" type="audio/mpeg" />
+                        Your browser does not support the audio element.
+                    </audio>
+                    <img src="./static/img/b.jpg"></img>
+                </div>
+            </div>
         );
     }
 }
